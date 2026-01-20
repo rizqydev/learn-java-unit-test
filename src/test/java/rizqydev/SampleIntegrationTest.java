0b545c2d5171ad9faeef05761b72b05cba0ad3b0
Rizqy <rizqy.dev@gmail.com>
2026-01-20 13:15:45 +0700

    unit test using tag

---
+++ src/test/java/rizqydev/SampleIntegrationTest.java
@@ -0,0 +1,19 @@
+package rizqydev;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+    @Test
+    void tes1() {
+    }
+
+    @Test
+    void test2() {
+
+    }
+}
