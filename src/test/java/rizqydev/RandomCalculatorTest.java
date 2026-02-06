e790032ce351d2440c961b0da31dd76f2bd6fc19
Rizqy <rizqy.dev@gmail.com>
2026-02-06 14:08:39 +0700

    learn dependency injection

---
+++ src/test/java/rizqydev/RandomCalculatorTest.java
@@ -0,0 +1,39 @@
+package rizqydev;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import rizqydev.resolver.RandomParameterResolver;
+import rizqydev.test.Calculator;
+
+import java.util.Random;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class RandomCalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a, b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void testRandom2(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+        var result = calculator.divide(a, b);
+        var expected = a / b;
+
+        Assertions.assertEquals(result, expected);
+    }
+}
