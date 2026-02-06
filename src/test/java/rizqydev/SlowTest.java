037f25c57f0b7f41b5b516acbe67a9ed9f4921d9
Rizqy <rizqy.dev@gmail.com>
2026-02-06 15:03:03 +0700

    learn parallel test

---
+++ src/test/java/rizqydev/SlowTest.java
@@ -0,0 +1,29 @@
+package rizqydev;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.util.concurrent.TimeUnit;
+
+@Execution(value = ExecutionMode.CONCURRENT)
+public class SlowTest {
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow1() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow2() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow3() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+}
